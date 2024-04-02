fa77b77d5195e5ba489db4474fa2d55bade78efa
rodneyrick <144851417+rodneyrick2rp@users.noreply.github.com>
2024-04-02 14:11:25 -0300

    chore(vscode): Inclusao de DevContainer e configura de .vscode

---
+++ .vscode/extensions/installExtensions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# export http_proxy=http://proxyprdusr.cip.lan.com:8080
+
+# Lista de extensões a serem instaladas
+EXTENSIONS=(
+    "Gruntfuggly.todo-tree"
+    "yy0931.save-as-root"
+    "humao.rest-client"
+    "mhutchie.git-graph"
+    "mohsen1.prettify-json"
+    "ms-python.python"
+    "ms-python.vscode-pylance"
+    "ms-toolsai.datawrangler"
+    "ms-toolsai.jupyter-keymap"
+    "ms-toolsai.jupyter"
+    "ms-vscode.sublime-keybindings"
+    "njpwerner.autodocstring"
+    "redhat.vscode-yaml"
+    "vscode-icons-team.vscode-icons"
+)
+
+VSCODE_SERVER_ID=$(ls ~/.vscode-server/bin)
+
+for EXTENSION in "${EXTENSIONS[@]}"; do 
+    echo "Install Extension: $EXTENSION"
+    ~/.vscode-server/bin/"$VSCODE_SERVER_ID"/bin/code-server --install-extension $EXTENSION
+done
