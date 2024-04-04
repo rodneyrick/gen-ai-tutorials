d49d94957d24d479eb3bff18eebe5cfdbde4469d
lucasBritoo <britol599@gmail.com>
2024-04-04 21:20:29 +0000

    docs(repo_metrics.py): adicionando docstring no utilitario

--- app/repo_metrics.py
+++ app/repo_metrics.py
@@ -1,3 +1,21 @@
+"""
+Utilitário para geração de insights usando métricas do sonarqube e github em um modelo LLM Ollama.
+
+Os parâmetros de configuração estão descritos abaixo:
+
+org_or_user = Nome da organização ou usuário do github. Este é o autor do repositório que deseja analisar
+repository_name = Nome do repositório que deseja analisar
+
+sonar_token = Token de acesso ao sonarqube  <https://docs.sonarsource.com/sonarqube/9.8/user-guide/user-account/generating-and-using-tokens/>
+git_token = Token de acesso pessoal ao github <https://docs.github.com/pt/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens>
+
+ollama_url = Url de acesso a API do Ollama no qual o modelo esta sendo executado.
+ollama_model = Nome do modelo usado.
+
+
+"""
+
+
 import dotenv
 from app.github import ScientificEvaluation
 from app.sonar_evaluations import SonarEvaluations
