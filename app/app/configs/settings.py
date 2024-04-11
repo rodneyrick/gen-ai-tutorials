92ea2bc7c6b45787f6bb5ae3becb16e37c201d68
rodneyrick <rodneyrick@gmail.com>
2024-04-11 10:06:42 -0300

    feat(configs): Inclusão de módulo de Configuração padrão

---
+++ app/app/configs/settings.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+import logging
+
+LOG_FORMAT_DEFAULT = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
+logging.basicConfig(level=logging.DEBUG,format=LOG_FORMAT_DEFAULT)
+logging.getLogger('urllib3.connectionpool').setLevel(logging.INFO)
+logging.getLogger('httpcore.http11').setLevel(logging.INFO)
+logging.getLogger('httpcore.connection').setLevel(logging.INFO)
+logging.getLogger('httpx').setLevel(logging.INFO)
+logging.getLogger('openai._base_client').setLevel(logging.INFO)
