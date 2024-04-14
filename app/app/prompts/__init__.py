6ac4c48973a19c0bd9dd63422a0037e6812e78cc
rodneyrick <rodneyrick@gmail.com>
2024-04-14 00:48:39 -0300

    feat(prompts): Pacote de Prompts

---
+++ app/app/prompts/__init__.py
@@ -0,0 +1,11 @@
+from .prompts_templates_models import (
+    PromptTemplateModel, 
+    PromptsTemplateModel, 
+    create_prompt_list
+)
+
+__all__ = [
+    "PromptTemplateModel", 
+    "PromptsTemplateModel", 
+    "create_prompt_list"
+]
